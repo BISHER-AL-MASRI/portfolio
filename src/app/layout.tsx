58e33dd782e42e2b1418896f40dab5c0f492028a
BISHER AL MASRI <bbgyttv@gmail.com>
2025-01-02 21:06:33 +0400

    fixed speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export const metadata: Metadata = {
   title: "Bisher Al Masri",
